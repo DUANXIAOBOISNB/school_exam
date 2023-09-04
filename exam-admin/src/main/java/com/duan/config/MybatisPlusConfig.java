d8dd78ad0ad18243265fa468e21281fef62c590e
duan <a15949687262@163.com>
2023-09-04 23:32:11 +0800

    by duan

--- exam-admin/src/main/java/com/duan/config/MybatisPlusConfig.java
+++ exam-admin/src/main/java/com/duan/config/MybatisPlusConfig.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-
+/**
+ * duan
+ */
 @MapperScan("com.duan.mapper")
 @Configuration
 public class MybatisPlusConfig {
