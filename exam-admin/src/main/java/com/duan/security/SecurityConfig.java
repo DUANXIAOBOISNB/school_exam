f147717902f2b1dce317aed63b2880873826bf60
duan <a15949687262@163.com>
2023-09-04 23:29:00 +0800

    by duan

--- exam-admin/src/main/java/com/duan/security/SecurityConfig.java
+++ exam-admin/src/main/java/com/duan/security/SecurityConfig.java
@@ -29,26 +29,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/teacher/**").hasAnyAuthority("teacher", "admin")
                 .mvcMatchers("/admin/**").hasAnyAuthority("admin")
                 // 配合下面的web.ignore 将下面所有的路径的校验都过滤掉了
-                .mvcMatchers("/util/**", "/common/**", "/actuator/**", "/api/**","/public/**").permitAll()
-
+                    .mvcMatchers("/util/**", "/common/**", "/actuator/**", "/api/**","/public/**").permitAll()
                 .anyRequest().authenticated()
-
                 .and()
-
                 // token认证
                 .addFilterBefore(new TokenAuthFilter(authenticationManager()), BasicAuthenticationFilter.class)
-
                 // 认证异常处理器
                 .exceptionHandling()
                 .accessDeniedHandler(new MyAccessDeniedHandler())
                 .authenticationEntryPoint(new MyAuthenticationEntryPointHandler())
-
                 .and().csrf().disable();// 默认csrf token 校验开启，针对 POST, PUT, PATCH
-
         http.formLogin().disable();
         // 前后端分离关闭配置登录
         http.logout().disable();
-
         // 所有的Rest服务一定要设置为无状态，以提升操作性能
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
