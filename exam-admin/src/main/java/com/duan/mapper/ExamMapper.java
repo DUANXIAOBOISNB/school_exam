a631863385f911cbdf8a68d4eeb9f22efccb74a6
duan <a15949687262@163.com>
2023-09-04 23:24:09 +0800

    by duan

--- exam-admin/src/main/java/com/duan/mapper/ExamMapper.java
+++ exam-admin/src/main/java/com/duan/mapper/ExamMapper.java
@@ -4,10 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.duan.entity.Exam;
 import org.springframework.stereotype.Repository;
 
-/**
- * @author by wzz
- * @implNote 2020/10/20 8:59
- */
+
 @Repository
 public interface ExamMapper extends BaseMapper<Exam> {
 }
